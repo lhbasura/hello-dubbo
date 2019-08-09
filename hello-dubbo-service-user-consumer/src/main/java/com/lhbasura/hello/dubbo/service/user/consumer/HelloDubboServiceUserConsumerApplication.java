cd1e6ece8e7fadacda83b1028db400c9541159f9
lhbasura <lhbasura@gmail.com>
2019-08-09 08:51:01 +0800

    hystrix仪表盘

--- hello-dubbo-service-user-consumer/src/main/java/com/lhbasura/hello/dubbo/service/user/consumer/HelloDubboServiceUserConsumerApplication.java
+++ hello-dubbo-service-user-consumer/src/main/java/com/lhbasura/hello/dubbo/service/user/consumer/HelloDubboServiceUserConsumerApplication.java
@@ -3,8 +3,10 @@ package com.lhbasura.hello.dubbo.service.user.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
 public class HelloDubboServiceUserConsumerApplication {
 
---
+++ hello-dubbo-service-user-consumer/src/main/java/com/lhbasura/hello/dubbo/service/user/consumer/config/HystrixConfiguration.java
@@ -0,0 +1,19 @@
+package com.lhbasura.hello.dubbo.service.user.consumer.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HystrixConfiguration {
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
